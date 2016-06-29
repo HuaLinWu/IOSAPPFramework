27cf40cf314d19dfed713d751139be3ce4c8e78d
吴华林(平安好房技术中心产品研发部IOS研发团队) <WUHUALIN093@pingan.com.cn>
2016-06-29 18:02:31 +0800

    添加缓存的webview的实现

--- IOSAPPFramework/Classes/Common/WebView/UIWebView+NSURLCache.h
+++ IOSAPPFramework/Classes/Common/WebView/UIWebView+NSURLCache.h
@@ -8,26 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-typedef NS_ENUM(NSInteger,UIWebViewCachePolicy)
-{
-    // 默认的缓存策略， 如果缓存不存在，直接从服务端获取。如果缓存存在，会根据response中的Cache-Control字段判断下一步操作，如: Cache-Control字段为must-revalidata, 则询问服务端该数据是否有更新，无更新的话直接返回给用户缓存数据，若已更新，则请求服务端.
-     UIWebViewUseProtocolCachePolicy,
-    //忽略本地缓存数据，直接请求服务端.
-     UIWebViewReloadIgnoringLocalCacheData,
-    //有缓存就使用，不管其有效性(即忽略Cache-Control字段), 无则请求服务端.
-     UIWebViewReturnCacheDataElseLoad,
-    // 死活加载本地缓存. 没有就失败. (确定当前无网络时使用)
-    UIWebViewReturnCacheDataDontLoad
-    
-};
-
-@interface UIWebViewCahe:UIWebView
-@property UIWebViewCachePolicy cachePolicy;
-@property (nonatomic,assign)NSInteger memoryCapacity;
-@property (nonatomic,assign)NSInteger diskCapacity;
-@property (nonatomic,copy)  NSString    *diskPath;
-@property (nonatomic,assign)id <UIWebViewDelegate>cacheDelegate;
-- (instancetype)initWithMemoryCapacity:(NSUInteger)memoryCapacity diskCapacity:(NSUInteger)diskCapacity diskPath:(NSString *)path cachePolicy:(UIWebViewCachePolicy)cachePolicy;
+@interface UIWebView (NSURLCache)
+/**
+ *  加载地址
+ *
+ *  @param urlStr url 地址
+ */
+- (void)loadURL:(NSString *)urlStr;
 @end
 
 
--- IOSAPPFramework/Classes/Common/WebView/UIWebView+NSURLCache.m
+++ IOSAPPFramework/Classes/Common/WebView/UIWebView+NSURLCache.m
@@ -7,96 +7,136 @@
 //
 
 #import "UIWebView+NSURLCache.h"
-#import <objc/runtime.h>
-@interface UIWebViewCahe () <UIWebViewDelegate>
-@property(nonatomic,strong)NSURLCache *urlCache;
-@end
-@implementation UIWebViewCahe
-- (instancetype)initWithFrame:(CGRect)frame
+@implementation UIWebView (NSURLCache)
+- (void)loadURL:(NSString *)urlStr
 {
-    self = [super initWithFrame:frame];
-    if(self)
+    if(!urlStr)
     {
-      self.delegate = self;
+        return;
     }
-   return self;
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [self loadRequest:urlRequest];
 }
-- (instancetype)initWithMemoryCapacity:(NSUInteger)memoryCapacity diskCapacity:(NSUInteger)diskCapacity diskPath:(NSString *)path cachePolicy:(UIWebViewCachePolicy)cachePolicy
+- (void)loadRequest:(NSURLRequest *)request
 {
-    self = [super init];
-    if(self)
-    {
-        self.diskPath = path;
-        self.memoryCapacity = memoryCapacity;
-        self.diskCapacity = diskCapacity;
-        self.cachePolicy  = cachePolicy;
-    }
-    return self;
-}
-#pragma mark UIWebViewDelegate
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
-{
-    if([self.cacheDelegate respondsToSelector:@selector(webView:shouldStartLoadWithRequest:navigationType:)])
-    {
-       return [self.cacheDelegate webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
-    }
-    return YES;
-}
-- (void)webViewDidStartLoad:(UIWebView *)webView
-{
-   if([self.cacheDelegate respondsToSelector:@selector(webViewDidStartLoad:)])
-    {
-        [self.cacheDelegate webViewDidStartLoad:webView];
-    }
-}
-- (void)webViewDidFinishLoad:(UIWebView *)webView
-{
-    NSLog(@"----222--->%@",[webView.request allHTTPHeaderFields]);
-    if([self.cacheDelegate respondsToSelector:@selector(webViewDidFinishLoad:)])
-    {
-        [self.cacheDelegate webViewDidFinishLoad:webView];
-    }
-}
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(nullable NSError *)error
-{
-    if([self.cacheDelegate respondsToSelector:@selector(webView:didFailLoadWithError:)])
-    {
-        [self.cacheDelegate webView:webView didFailLoadWithError:error];
-    }
+    [self downLoadWithRequest:request];
 }
 
-#pragma mark set/get
+/**
+ *  加载requst 对应的数据
+ */
+- (void)downLoadWithRequest:(NSURLRequest *)request
+{
+    NSURLRequest *tempRequset = [self configRequestWithRequest:request];
+    NSURLSessionDataTask *dataTask = [[NSURLSession sharedSession] dataTaskWithRequest:tempRequset completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if(error)
+        {
+          //加载失败时候
+          NSCachedURLResponse *cachedURLResponse = [self cachedURLResponseForRequest:tempRequset];
+         [self loadData:cachedURLResponse.data response:cachedURLResponse.response request:tempRequset];
+        }
+        else{
+            NSHTTPURLResponse *tempResponse = (NSHTTPURLResponse *)response;
+            if(tempResponse.statusCode==200){
+                //表示200成功
+                NSURLCacheStoragePolicy cacheStoragePolicy = [self cacheStoragePolicyWithResponse:response];
+            if(cacheStoragePolicy == NSURLCacheStorageAllowed){
+              //表示需要缓存
+              [self storeCacheForRequest:tempRequset response:response data:data];
+              }
+            [self loadData:data response:response request:tempRequset];
+        }
+        else if (tempResponse.statusCode ==304){
+            //表示没有更新直接读取缓存就可以了
+           NSCachedURLResponse *cachedURLResponse = [self cachedURLResponseForRequest:tempRequset];
+          [self loadData:cachedURLResponse.data response:cachedURLResponse.response request:tempRequset];
+         }
+        }
+    }];
+    [dataTask resume];
+}
 
-- (NSURLCache *)urlCache
+/**
+ *  加载data到webview
+ */
+
+- (void)loadData:(NSData *)data response:(NSURLResponse *)response request:(NSURLRequest *)request
 {
-    if(!_urlCache)
-    {
-      _urlCache = [[NSURLCache alloc] initWithMemoryCapacity:self.memoryCapacity diskCapacity:self.diskCapacity diskPath:self.diskPath];
-    }
-    return _urlCache;
+     [self loadData:data MIMEType:response.MIMEType textEncodingName:response.textEncodingName baseURL:request.URL];
 }
-- (NSInteger)diskCapacity
+/**
+ *  根据requst 来查询缓存然后配置上次的response 的相关的信息来获取配置新的request
+ */
+- (NSURLRequest *)configRequestWithRequest:(NSURLRequest *)request{
+    
+    NSCachedURLResponse *cachedURLResponseresponse = [self cachedURLResponseForRequest:request];
+    if(cachedURLResponseresponse){
+     NSHTTPURLResponse *response =(NSHTTPURLResponse *)cachedURLResponseresponse.response;
+     NSMutableURLRequest *mutableURLRequest = [[NSMutableURLRequest alloc] initWithURL:request.URL];
+     [mutableURLRequest setValuesForKeysWithDictionary:request.allHTTPHeaderFields];
+     mutableURLRequest.cachePolicy = request.cachePolicy;
+     mutableURLRequest.timeoutInterval      = request.timeoutInterval;
+     mutableURLRequest.mainDocumentURL      = request.mainDocumentURL;
+     mutableURLRequest.networkServiceType   = request.networkServiceType;
+     mutableURLRequest.allowsCellularAccess = request.allowsCellularAccess;
+     NSString *cacheControl = [response.allHeaderFields objectForKey:@"Cache-Control"];
+    [mutableURLRequest setValue:cacheControl forHTTPHeaderField:@"Cache-Control"];
+     NSString *ifMatch = [response.allHeaderFields objectForKey:@"ETag"];
+    [mutableURLRequest setValue:ifMatch forHTTPHeaderField:@"If-Match"];
+    NSString *ifModifiedSince = [response.allHeaderFields objectForKey:@"Last-Modified"];
+    [mutableURLRequest setValue:ifModifiedSince forHTTPHeaderField:@"If-Modified-Since"];
+    NSString *ifNoneMatch = [response.allHeaderFields objectForKey:@"ETag"];
+    [mutableURLRequest setValue:ifNoneMatch forHTTPHeaderField:@"If-None-Match"];
+    NSString *ifRange = [response.allHeaderFields objectForKey:@"ETag"];
+    [mutableURLRequest setValue:ifRange forHTTPHeaderField:@"If-Range"];
+    return mutableURLRequest;
+    }
+    return request;
+}
+/**
+ *  获取requst对应的缓存
+ *
+ */
+- (NSCachedURLResponse *)cachedURLResponseForRequest:(NSURLRequest *)request
 {
-    if(_diskCapacity==0)
-    {
-      _diskCapacity = 20*1024*1024;
-    }
-    return _diskCapacity;
+     NSCachedURLResponse *cachedURLResponse =[[NSURLCache sharedURLCache] cachedResponseForRequest:request];
+    return cachedURLResponse;
 }
-- (NSInteger)memoryCapacity
+/**
+ *  删除requst 对应的缓存
+ *
+ */
+- (void)removeCacheForRequest:(NSURLRequest *)request
 {
-    if(_memoryCapacity ==0)
-    {
-      _memoryCapacity = 4*1024*1024;
-    }
-    return _memoryCapacity;
+    [[NSURLCache sharedURLCache] removeCachedResponseForRequest:request];
 }
-- (NSString *)diskPath
+/**
+ *  针对request 来存储response
+ */
+- (void)storeCacheForRequest:(NSURLRequest *)request response:(NSURLResponse *)response data:(NSData *)responseData
 {
-    if(!_diskPath)
-    {
-      _diskPath = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"webViewCache"];
-    }
-    return _diskPath;
+    NSCachedURLResponse *cachedURLResponse = [[NSCachedURLResponse alloc] initWithResponse:response data:responseData];
+    [[NSURLCache sharedURLCache] storeCachedResponse:cachedURLResponse forRequest:request];
 }
+
+/**
+ *  根据 NSURLResponse 对象获取缓存策略
+ */
+- (NSURLCacheStoragePolicy)cacheStoragePolicyWithResponse:(NSURLResponse *)response
+{
+    NSHTTPURLResponse *httpURLResponse = (NSHTTPURLResponse *)response;
+    NSDictionary *headerFieldsDict = [httpURLResponse allHeaderFields];
+    NSString *cache_control = [headerFieldsDict objectForKey:@"Cache-Control"];
+    if([cache_control rangeOfString:@"no-cache"].location != NSNotFound&&[cache_control rangeOfString:@"no-store"].location != NSNotFound)
+    {
+        return NSURLCacheStorageNotAllowed;
+    }
+    else
+    {
+        return NSURLCacheStorageAllowed;
+    }
+    return  NSURLCacheStorageNotAllowed;
+}
+
 @end
